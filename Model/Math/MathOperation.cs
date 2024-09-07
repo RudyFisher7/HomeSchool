47cdb00b2c6b1a443b46a02521188d32f8105087
kiyasui-hito <rfisherd7@gmail.com>
2024-09-07 17:10:23 -0500

    Add project files.

---
+++ Model/Math/MathOperation.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Model.Math
+{
+	public enum MathOperation
+	{
+		[Display(Name = "+")]
+		ADD,
+		[Display(Name = "-")]
+		SUBTRACT,
+		[Display(Name = "x")]
+		MULTIPLY,
+		[Display(Name = "/")]
+		DIVIDE,
+	};
+}
---
+++ Model/Math/MathProblem.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Model.Math
+{
+    public class MathProblem
+    {
+        public int Id { get; set; }
+        [Display(Name = "Answere Boyyyyyy")]
+        public double Answer { get; set; }
+
+        //[Required]//value types are inherently required
+        [Range(-100.0, 100.0)]
+        public double Factor1 { get; set; }
+        public double Factor2 { get; set; }
+        public MathOperation Operation { get; set; }
+
+        [StringLength(100, MinimumLength = 0)]
+        public string Description { get; set; } = string.Empty;
+    }
+}
