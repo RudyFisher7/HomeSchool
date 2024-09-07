da7afd9d497e6fc0d484fc30509e672effb51e0f
kiyasui-hito <rfisherd7@gmail.com>
2024-09-07 17:15:33 -0500

    resolve compiler errors

--- Main/Program.cs
+++ Main/Program.cs
@@ -1,22 +1,12 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using Main.Data;
-using Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-builder.Services.AddDbContext<MathSqlContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("MathSqlContext") ?? throw new InvalidOperationException("Connection string 'MathSqlContext' not found.")));
 
 var app = builder.Build();
-
-using (var scope = app.Services.CreateScope())
-{
-    var services = scope.ServiceProvider;
-    SeedData.Initialize(services);
-}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
